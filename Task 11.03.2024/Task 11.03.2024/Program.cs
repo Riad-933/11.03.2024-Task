7b9113c07852ad264072e0e43275c8b8be52f50c
Riad <user@WIN-9OFCQG35VUH>
2024-03-10 21:55:42 +0400

    first commit

---
+++ Task 11.03.2024/Task 11.03.2024/Program.cs
@@ -0,0 +1,69 @@
+﻿
+//  Task 1 ----->
+
+//int eded = 12345;
+//int hesab = 1;
+
+//while (hesab <= eded)
+//{
+//    hesab *= eded % 10;
+//    eded /= 10;
+//}
+//Console.WriteLine(hesab);            
+
+//  Task 2 ----->
+
+//int eded = 3;
+//int a = 0;
+//for (int i = 2; i < eded; i++)
+//{
+//    if (eded % i == 0)
+//    {
+//        a++;
+//    }
+//}
+//if (a == 0)
+//{
+//    Console.WriteLine("This is Sade");
+//}
+//else
+//{
+//    Console.WriteLine("This is Murekkeb");
+//}
+
+//  Task 3 ----->
+
+//int eded = 5;
+
+//int factorial = 1;
+// if ( eded >= 0 )
+//{
+//for (int a = 1; a <= eded; a++)
+//{
+//    factorial *= a;
+//}
+//}
+//Console.WriteLine(factorial);
+
+//  Task 4 -----> Verilmiş ədədin polindrom olub olmadığını tapan alqoritm 
+
+//int eded = 121;
+//int ededCopy = eded;
+//int ededTers = 0;
+//while (eded > 0)
+//{
+//    int i = eded % 10;
+//    ededTers = (ededTers * 10) + i;
+//    eded /= 10;
+
+
+//}
+//if (ededCopy == ededTers)
+//{
+//    Console.WriteLine("polindromdur");
+//}
+//else
+//{
+//    Console.WriteLine("polindrom deyil");
+
+//}
